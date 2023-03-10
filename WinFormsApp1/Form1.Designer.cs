52a8954dd85cb132fa98aeecda2107297f9247cf
guangbo <guangbo@DESKTOP-USLQHV3>
2023-03-10 17:00:22 +0800

    11

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "生成类";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(152, 149);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(205, 27);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(152, 197);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(205, 27);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(152, 240);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(205, 27);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "收缩名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "注释名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "类名";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(56, 324);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 48);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "打开模板";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(478, 275);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(212, 48);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "生成方法";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(478, 145);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(296, 27);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(391, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "方法作用";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(391, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "方法名";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(478, 195);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(296, 27);
+            this.textBox5.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox4;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox5;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,148 @@
+using Flurl.Http;
+using JinianNet.JNTemplate;
+using Microsoft.VisualBasic.ApplicationServices;
+using Microsoft.VisualBasic.Logging;
+using System.Diagnostics;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1_Click2();
+            Engine.UseInterpretationEngine();
+            Engine.Configure((c) =>
+            {
+                c.OutMode = OutMode.Auto;
+            });
+            var ddd = File.ReadAllText("1.txt");
+            var template = Engine.CreateTemplate(ddd);
+            List<sss> values = new List<sss>();
+            var sss = File.ReadAllLines("3.txt");
+            for (int i = 0; i < sss.Length; i++)
+            {
+                string dsdsa = sss[i].Replace("\t\t", "\t");
+                string[] ddd3423 = dsdsa.Split("\t");
+                if (ddd3423[1] == "String") ddd3423[1] = ddd3423[1].ToLower();
+                if (ddd3423[1] == "Boolean") ddd3423[1] = "bool";
+                values.Add(new sss { type = ddd3423[1], Name = ddd3423[0], despriton = ddd3423[2] });
+            }
+            template.Set("list", values);
+            template.Set("ZhushiS", textBox1.Text);
+            template.Set("Zhushi", textBox2.Text);
+            template.Set("Name", textBox3.Text);
+            var result = template.Render();
+            File.WriteAllText("2.txt", result);
+            Process vProcess = Process.Start("notepad.exe", "2.txt");
+        }
+        private void button1_Click2()
+        {
+            Engine.UseInterpretationEngine();
+            Engine.Configure((c) =>
+            {
+                c.OutMode = OutMode.Auto;
+            });
+            var ddd = File.ReadAllText("C:\\Users\\guangbo\\Desktop\\logs.html");
+            var template = Engine.CreateTemplate(ddd);
+            List<htmllog> values = new List<htmllog>();
+            var sss = File.ReadAllLines("logshonghui.txt");
+            int falg = 0;
+            for (int i = 0; i < sss.Length; i++)
+            {
+                if (sss[i].Contains("---"))
+                {
+                    falg = i;
+                    string[] miancontent = sss[i-falg].Split('-');//»ù´¡ÐÅÏ¢
+                    var db =new htmllog();
+                    db.Vseon = "1";
+                    for (int m = 0; m < 2 * falg - i; m++)
+                    {
+                        db.Detail.Add(sss[i - falg + 1]);
+                    }
+                    if(db!=null)
+                    values.Add(db);
+                }
+                if (i == 0)
+                {
+                    string[] miancontent = sss[i].Split('-');
+                    template.Set("Year", miancontent[0]);
+                    template.Set("Day", "12332");
+                    template.Set("Name", "12332");
+                    continue;
+                }
+                string dsdsa = sss[i].Replace("\t\t", "\t");
+                string[] ddd3423 = dsdsa.Split("\t");
+            
+            }
+            template.Set("list", values);
+
+            template.Set("Name", "12332");
+            var result = template.Render();
+            File.WriteAllText("C:\\Users\\guangbo\\Desktop\\logs2.html", result);
+        }
+        public class htmllog
+        {
+            public string? Day { get; set; }
+            public string? Vseon { get; set; }
+            public string? Vseodescr { get; set; }
+            public List<string>? Detail { get; set; }
+
+        }
+
+        public class sss
+        {
+            public string? type { get; set; }
+            public string? Name { get; set; }
+            public string? despriton { get; set; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Process vProcess = Process.Start("notepad.exe", "3.txt");
+        }
+
+        private async void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            textBox5.Text = await Task.Run(() =>
+            {
+                string base_url_one = "https://yuanxiapi.cn/api/translation/?text=" + textBox4.Text;
+                var reslutdata1 = base_url_one.GetJsonAsync().Result;
+               string data= reslutdata1.result;
+                data= CultureInfo.CurrentCulture.TextInfo.ToTitleCase(data.ToLower());
+                data = data.Replace(" ", "").Replace(".", "").Replace("-", "").Replace
+                (",", "");
+                return data;
+            });
+            textBox1.Text = textBox4.Text;
+            textBox2.Text = textBox4.Text;
+        }
+   
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Engine.UseInterpretationEngine();
+            Engine.Configure((c) =>
+            {
+                c.OutMode = OutMode.Auto;
+            });
+            var ddd = File.ReadAllText("fun.txt");
+        var template = Engine.CreateTemplate(ddd);
+            template.Set("ZhushiS", textBox1.Text);
+            template.Set("Zhushi", textBox2.Text);
+            template.Set("FunName", textBox5.Text);
+            var result = template.Render();
+            File.WriteAllText("2.txt", result);
+            Process vProcess = Process.Start("notepad.exe", "2.txt");
+            Clipboard.SetDataObject(result);
+        }
+    }
+}
