7bf768a6898e2209065df02e278ea4bfeb5d2f15
guangbo <guangbo@DESKTOP-USLQHV3>
2023-06-01 16:29:34 +0800

    模式加入

---
+++ StoneCodeGenerator.Service/DI/IScopeDependency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneCodeGenerator.Service.DI
+{
+    /// <summary>
+    /// 当前请求作用域内  只有当前这个实例
+    /// </summary>
+    public interface IScopeDependency: IDependency
+    {
+    }
+}
