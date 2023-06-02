058e5e8797338c2e4587398318a8c9022fd5f2a6
guangbo <guangbo@DESKTOP-USLQHV3>
2023-06-02 11:57:25 +0800

    增加扩展类实体

---
+++ StoneCodeGenerator.wpf/Model/PlusModel .cs
@@ -0,0 +1,34 @@
+﻿using HandyControl.Data;
+using System.Collections.Generic;
+
+namespace HandyControlDemo.Model
+{
+    /// <summary>
+    /// 扩展类-存储扩展功能基本信息
+    /// </summary>
+    public class PlusClassModel: PlusBaseModel
+    {
+        /// <summary>
+        /// 方法
+        /// </summary>
+        public List<PlusMethodModel> SenderId { get; set; }
+    }
+    /// <summary>
+    /// 扩展方法功能基本信息
+    /// </summary>
+    public class PlusMethodModel : PlusBaseModel
+    {
+
+    }
+    public class PlusBaseModel 
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 作用显示文本
+        /// </summary>
+        public string NameDes { get; set; }
+    }
+}
