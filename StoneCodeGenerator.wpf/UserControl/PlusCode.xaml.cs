a4fe74b4d8274ee74a949a08743c7df936640c72
ll <liu@DESKTOP-C8G1C3H>
2023-06-03 22:53:36 +0800

    优化ui

--- StoneCodeGenerator.wpf/UserControl/PlusCode.xaml.cs
+++ StoneCodeGenerator.wpf/UserControl/PlusCode.xaml.cs
@@ -1,9 +1,16 @@
-﻿using HandyControl.Tools.Extension;
+﻿using HandyControl.Controls;
+using HandyControl.Data;
+using HandyControl.Tools.Extension;
+using HandyControlDemo.Model;
 using ICSharpCode.AvalonEdit;
 using System;
+using System.Collections.Generic;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using ComboBox = HandyControl.Controls.ComboBox;
 
 namespace HandyControlDemo.UserControl
 {
@@ -22,9 +29,11 @@ namespace HandyControlDemo.UserControl
         private void PlusToUi()
         {
             App.plusInterfaceModels.ForEach(model => {
-                model.Methods.ForEach(method => {
-                    contents.Children.Add(GetButton(model.Name,method.Name, method.NameDes));
-                });
+                contents.Children.Add(GetComboBox(model.NameDes, model.Name,model.Methods));
+
+                //model.Methods.ForEach(method => {
+                //    contents.Children.Add(GetComboBox(model.Name,method.Name, method.NameDes));
+                //});
             });  
         }
         private Button GetButton(string interfacename,string name, string namedes, string currentent = "")
@@ -37,6 +46,46 @@ namespace HandyControlDemo.UserControl
             button.Content = namedes;
             button.Click += Button_Click; ;
             return button;
+        }
+        private ComboBox GetComboBox(string title,string interfacename, List<PlusMethodModel> conent, bool IsEditable = false, string currentent = "")
+        {
+            ComboBox comboBox = new ComboBox();
+            comboBox.Name = interfacename;
+            comboBox.IsEditable = IsEditable;
+            comboBox.SetValue(TitleElement.TitleWidthProperty, new GridLength(100));
+            comboBox.SetValue(TitleElement.TitlePlacementProperty, TitlePlacementType.Left);
+            comboBox.SetValue(TitleElement.TitleProperty, title);
+            // comboBox.SetValue(InfoElement.PlaceholderProperty, "L222e");
+            comboBox.Margin = new Thickness(0, 5, 0, 0);
+
+            comboBox.ItemsSource = conent;
+            comboBox.DisplayMemberPath = "NameDes";
+            comboBox.SelectedValuePath = "Name";
+            if (IsEditable)
+                comboBox.Text = currentent;
+            else
+                comboBox.SelectedValue = currentent;
+            comboBox.SelectedIndex = 0;
+            comboBox.PreviewMouseDown += PreviewMouseDownChanged;
+            comboBox.SelectionChanged += Selection_Changed;
+            comboBox.Foreground = new SolidColorBrush(Colors.Gray);
+             comboBox.SetValue(StyleProperty, Application.Current.Resources["ComboBoxExtend"]);
+            return comboBox;
+        }
+        private void PreviewMouseDownChanged(object sender, MouseButtonEventArgs e)
+        {
+        }
+        private void Selection_Changed(object sender, SelectionChangedEventArgs e)
+        {
+            var sc = sender as ComboBox;
+            var dd = App.GetServiceByString(sc.Name);
+            var select= sc.SelectedItem as PlusMethodModel;
+            Type test = dd.GetType();
+            MethodInfo mi1 = test.GetMethod(select.Name);
+            string text = TextEditor.Text;
+            object[] parameters = new object[1] { text };
+            string dds = (string)mi1.Invoke(dd, parameters);
+            TextEditor.Text = dds;
         }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
