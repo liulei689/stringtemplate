cbce2dfe6742b0b6d5c780cf95469ce57c8cd829
guangbo <guangbo@DESKTOP-USLQHV3>
2023-06-20 17:27:06 +0800

    增加word模板导出

---
+++ StoneCodeGenerator.Service/Interface/IToWordService.cs
@@ -0,0 +1,30 @@
+﻿using StoneCodeGenerator.Interface.DI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoneCodeGenerator.Service.Interface
+{
+    [Description("miniexcel生成word")]
+    public interface IToWordService : ISingletonDependency
+    {
+        [Description("替换字符")]
+        /// <summary>
+        /// 复制excel第一行生产类
+        /// </summary>
+        /// <returns></returns>
+        public string GetClassByExcelRowOne(string excelrowone);
+        [Description("测试1")]
+        public string GetSting1(string excelrowone);
+        [Description("测试2")]
+
+        public string GetSting2(string excelrowone);
+        [Description("测试3")]
+
+        public string GetSting3(string excelrowone);
+
+    }
+}
---
+++ StoneCodeGenerator.Service/Services/ToWordService.cs
@@ -0,0 +1,41 @@
+﻿using MiniSoftware;
+using StoneCodeGenerator.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StoneCodeGenerator.Service.Services
+{
+  
+    public class ToWordService : IToWordService
+    {
+        public string GetClassByExcelRowOne(string excelrowone)
+        {
+            var value = new Dictionary<string, object>()
+            {
+                ["tag"] = new MiniWordPicture() { Path = "C:\\Users\\guangbo\\Pictures\\新建文件夹\\周涛-21520601106.jpg", Width = 40, Height = 40 }
+            };
+            MiniSoftware.MiniWord.SaveAsByTemplate("C:\\Users\\guangbo\\Videos\\2.docx", "C:\\Users\\guangbo\\Videos\\1.docx", value);
+            return "";
+        }
+
+        public string GetSting1(string excelrowone)
+        {
+            return "1";
+        }
+
+        public string GetSting2(string excelrowone)
+        {
+            return "2";
+        }
+
+        public string GetSting3(string excelrowone)
+        {
+            return "sdasdasdasd";
+        }
+    }
+}
